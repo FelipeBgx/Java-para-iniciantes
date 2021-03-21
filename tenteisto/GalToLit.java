ced079fc8d55847f94e4e941d14d887446d9f177
Felipe Borges <f3lpborg3s@gmail.com>
2021-03-21 16:14:05 -0300

    Adicionando Tente Isto 1-1

---
+++ tenteisto/GalToLit.java
@@ -0,0 +1,13 @@
+package tenteisto;
+
+class GalToLit {
+    public static void main(String[] args) {
+        double galoes;
+        double litros;
+
+        galoes = 10;
+        litros = galoes * 3.7854;
+
+        System.out.println(galoes + " galões é " + litros + " litros.");
+    }
+}
