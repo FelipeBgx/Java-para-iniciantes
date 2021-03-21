888ddd65081c8e772d898823ce3f63fef086005c
Felipe Borges <f3lpborg3s@gmail.com>
2021-03-21 14:36:16 -0300

    adicionando exemplos do primeiro capitulo

---
+++ examples/Example2.java
@@ -0,0 +1,12 @@
+package examples;
+
+public class Example2 {
+    public static void main(String[] args) {
+        int var1 = 1024;
+        int var2 = var1 / 2;
+
+        System.out.println("var1 contém " + var1);
+        System.out.print("var2 contém var1 / 2 = ");
+        System.out.println(var2);
+    }
+}
---
+++ examples/Example3.java
@@ -0,0 +1,19 @@
+package examples;
+
+public class Example3 {
+    public static void main(String[] args) {
+        int var = 10;
+        double x = 10.0; 
+
+        System.out.println("Valor original de var : " + var);
+        System.out.println("Valor oringinal de x : " + x);
+        System.out.println();
+
+        var /= 4;
+        x /= 4;
+
+        System.out.println("var depois da divisão : " + var);
+        System.out.println("x depois da divisão : " + x);
+        
+    }
+}
